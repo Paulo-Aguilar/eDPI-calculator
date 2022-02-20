e13065cc1c8080fef1b78e72e3f92262dcb09bc2
Paulo Aguilar <paulo.aguilar92@gmail.com>
2022-02-20 15:50:09 +0800

    changed CSS

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
-// const edpi = document.getElementById("edpi");
-
 function calculator (){
     const mouse = parseFloat(document.getElementById("mouse").value);
     const sensitivity = parseFloat(document.getElementById("sensitivity").value); 
-    const game = document.getElementById('game').value;
 
     document.getElementById('edpi').value = mouse * sensitivity;
-   
 
-}
+    document.getElementById('submit').style.background = "#212529";
+
+}   
